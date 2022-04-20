b44d45caf20a530296f512bd60756726441ee777
ErgoFriend <erg.friend@gmail.com>
2022-04-21 08:59:28 +0900

    import vote

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 import {Link} from '@remix-run/react'
 import {Form} from 'remix-forms'
 
-import * as vote from './service/vote'
+import * as vote from '~/service/vote'
 
 const Page = () => {
   const {viewCount, voteData} = vote.useData()
@@ -15,7 +15,7 @@ const Page = () => {
       <p>たけのこの里: {voteData.takenoko ?? '0'}</p>
       <p>きのこの里: {voteData.kinoko ?? '0'}</p>
       <Form schema={vote.schema} />
-      <Link to={'first-post'}>mdx page: first-post</Link>
+      <Link to={'/posts/first-post'}>mdx page: first-post</Link>
     </div>
   )
 }
