628bd5f611df21cdebbd18c95a3c65fa5d441ac9
ErgoFriend <erg.friend@gmail.com>
2022-04-21 00:04:13 +0900

    add viewCount

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,13 +2,14 @@ import {Form} from 'remix-forms'
 import * as vote from './service/vote'
 
 const Page = () => {
-  const voteData = vote.useData()
+  const {viewCount, voteData} = vote.useData()
 
   return (
     <div>
       <h1 className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         Welcome to Remix
       </h1>
+      <p>表示回数: {viewCount ?? '0'}</p>
       <p>たけのこの里: {voteData.takenoko ?? '0'}</p>
       <p>きのこの里: {voteData.kinoko ?? '0'}</p>
       <Form schema={vote.schema} />
--- app/routes/service/vote.ts
+++ app/routes/service/vote.ts
@@ -9,12 +9,13 @@ const voteKey = ['takenoko', 'kinoko'] as const
 
 type VoteKey = typeof voteKey[number]
 
-type PageCache = {
+type VoteData = {
   [key in VoteKey]: number
 }
 
-type LoaderData = {
-  pageCache: PageCache
+type PageData = {
+  viewCount: number
+  voteData: VoteData
 }
 
 const defaultVoteData = {takenoko: 0, kinoko: 0}
@@ -27,10 +28,13 @@ export const action: ActionFunction = async ({context, request}) => {
   const {remix_cloudflare_pages_kv} = context
   const mutation = makeDomainFunction(schema)(async values => {
     const {like} = values
-    const pageCached = (await remix_cloudflare_pages_kv.get<PageCache>(pageCacheKey, 'json')) ?? defaultVoteData
-    const updateData = {
+    const pageCached = await remix_cloudflare_pages_kv.get<Partial<PageData>>(pageCacheKey, 'json')
+    const updateData: Partial<PageData> = {
       ...pageCached,
-      [like]: pageCached ? pageCached[like] + 1 : 1,
+      voteData: {
+        ...(pageCached?.voteData ?? defaultVoteData),
+        [like]: pageCached?.voteData ? pageCached.voteData[like] + 1 : 1,
+      },
     }
     await remix_cloudflare_pages_kv.put(pageCacheKey, JSON.stringify(updateData))
   })
@@ -44,13 +48,18 @@ export const action: ActionFunction = async ({context, request}) => {
 }
 
 export const useData = () => {
-  const {pageCache} = useLoaderData<LoaderData>()
-  return pageCache
+  const pageData = useLoaderData<PageData>()
+  return pageData
 }
 
-export const loader: LoaderFunction = async ({context}): Promise<LoaderData> => {
+export const loader: LoaderFunction = async ({context}): Promise<PageData> => {
   const {remix_cloudflare_pages_kv} = context
 
-  const pageCached = await remix_cloudflare_pages_kv.get<PageCache>(pageCacheKey, 'json')
-  return {pageCache: pageCached ?? defaultVoteData}
+  const pageCached = await remix_cloudflare_pages_kv.get<Partial<PageData>>(pageCacheKey, 'json')
+  const pageData: PageData = {
+    ...pageCached,
+    viewCount: pageCached?.viewCount ?? 1,
+    voteData: pageCached?.voteData ?? defaultVoteData,
+  }
+  return pageData
 }
