100eeeb218453ae06a6f3b6b94ce0c4d005453d1
ErgoFriend <erg.friend@gmail.com>
2022-04-23 00:04:38 +0900

    try patch

--- app/service/vote.ts
+++ app/service/vote.ts
@@ -1,6 +1,7 @@
 import {createCookieSessionStorage} from '@remix-run/cloudflare'
 import {useLoaderData} from '@remix-run/react'
 import {json} from '@remix-run/server-runtime'
+import {makeDomainFunction} from 'remix-domains'
 import {formAction} from 'remix-forms'
 import Toucan from 'toucan-js'
 import {z} from 'zod'
@@ -62,40 +63,67 @@ export const action: ActionFunction = async ({context, request}) => {
     dsn: context.env.SENTRY_DSN,
     context: context,
   })
+  const formData = await request.clone().formData()
+  const data = new URLSearchParams(formData as URLSearchParams).toString()
+  const json = JSON.stringify(Object.fromEntries(formData.entries()))
+  const log = `action URLSearchParams: ${JSON.stringify(data)} json: ${json}`
+  console.log(log)
+  sentry.captureMessage(log)
 
-  // formAction は beforeAction が設定されたことで early-return している
-  // formAction を使っている意味は無いが、問題が修正されたら戻したい
-  // https://github.com/SeasonedSoftware/remix-domains/issues/18
-  // https://github.com/SeasonedSoftware/remix-forms/blob/main/src/formAction.server.ts#L67
+  const mutation = makeDomainFunction(schema)(async values => {
+    sentry.captureMessage(`values: ${JSON.stringify(values)}`)
+
+    const {like} = values
+    const pageCached = await getData(context.env)
+    const updateData: PageData = {
+      ...pageCached,
+      voteData: {
+        ...pageCached.voteData,
+        [like]: pageCached.voteData[like] + 1,
+      },
+    }
+    await setData(context.env, updateData)
+
+    const session = await getSession(request.headers.get('Cookie'))
+    const isVoted = session.get('isVoted') as boolean
+    !isVoted && session.set('isVoted', true)
+    return values
+  })
+
   return await formAction({
     request,
     schema,
-    mutation: (() => {}) as any,
-    beforeAction: async req => {
-      const formData = await req.clone().formData()
-      const values = Object.fromEntries(formData.entries()) as z.infer<typeof schema>
-      sentry.captureMessage(`action: ${JSON.stringify(values)}`)
+    mutation,
+    // formAction は beforeAction が設定されたことで early-return している
+    // formAction を使っている意味は無いが、問題が修正されたら戻したい
+    // https://github.com/SeasonedSoftware/remix-domains/issues/18
+    // https://github.com/SeasonedSoftware/remix-forms/blob/main/src/formAction.server.ts#L67
+    // mutation: (() => {}) as any,
+    // beforeAction: async req => {
+    //   const formData = await req.clone().formData()
+    //   const values = Object.fromEntries(formData.entries()) as z.infer<typeof schema>
+    //   sentry.captureMessage(`action: ${JSON.stringify(values)}`)
 
-      const {like} = values
-      const pageCached = await getData(context.env)
-      const updateData: PageData = {
-        ...pageCached,
-        voteData: {
-          ...pageCached.voteData,
-          [like]: pageCached.voteData[like] + 1,
-        },
-      }
-      await setData(context.env, updateData)
+    //   const {like} = values
+    //   const pageCached = await getData(context.env)
+    //   const updateData: PageData = {
+    //     ...pageCached,
+    //     voteData: {
+    //       ...pageCached.voteData,
+    //       [like]: pageCached.voteData[like] + 1,
+    //     },
+    //   }
+    //   await setData(context.env, updateData)
 
-      const session = await getSession(req.headers.get('Cookie'))
-      const isVoted = session.get('isVoted') as boolean
-      !isVoted && session.set('isVoted', true)
-      return json(values, {
-        headers: {
-          'Set-Cookie': await commitSession(session),
-        },
-      })
-    },
+    //   const session = await getSession(req.headers.get('Cookie'))
+    //   const isVoted = session.get('isVoted') as boolean
+    //   !isVoted && session.set('isVoted', true)
+    //   return json(values, {
+    //     headers: {
+    //       'Set-Cookie': await commitSession(session),
+    //     },
+    //   })
+    // },
   })
 }
 
