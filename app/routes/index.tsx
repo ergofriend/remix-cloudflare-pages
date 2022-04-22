d25dbc14a5d97b859c1aefa99e12f8d82b4529ec
ErgoFriend <erg.friend@gmail.com>
2022-04-22 21:08:38 +0900

    add assets

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,8 @@ import {Link} from '@remix-run/react'
 import {Form} from 'remix-forms'
 
 import * as vote from '~/service/vote'
+import TakenokoImage from '~/assets/takenoko.png'
+import KinokoImage from '~/assets/kinoko.png'
 
 const Page = () => {
   const {viewCount, voteData} = vote.useData()
@@ -13,6 +15,8 @@ const Page = () => {
       </h1>
       <p>表示回数: {viewCount ?? '0'}</p>
       <p>たけのこの里: {voteData.takenoko ?? '0'}</p>
+      <img height={200} width={200} src={TakenokoImage} alt="たけのこの里" />
+      <img height={200} width={200} src={KinokoImage} alt="きのこの里" />
       <p>きのこの里: {voteData.kinoko ?? '0'}</p>
       <Form schema={vote.schema} />
       <Link to={'/posts/first-post'}>mdx page: first-post</Link>
